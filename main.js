18924aead2403d5ae6a3683063a8c797fc4b46e3
ryanglennsmith <ryan.glenn.smith@gmail.com>
2021-12-22 15:48:45 +0000

    problem created, tests written, failing

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 /* 
 👉 Write your kata here!
+Given an array: 
+
+['12 drummers drumming', '11 pipers piping', '10 lords a leaping', 
+'9 ladies dancing', '8 maids a milking', '7 swans a swimming', 
+'6 geese a laying', '5 golden rings', '4 calling birds', '3 French hens', 
+'2 turtle doves', '1 partridge in a pear tree']
+
+sort the array in to a list of lists for each day of Christmas: i.e., 
+[['1 partridge in a pear tree'],
+['1 partridge in a pear tree', '2 turtle doves'],
+...
+...'11 pipers piping','12 drummers drumming']]
+
 */
 
+const array = [
+  "12 drummers drumming",
+  "11 pipers piping",
+  "10 lords a leaping",
+  "9 ladies dancing",
+  "8 maids a milking",
+  "7 swans a swimming",
+  "6 geese a laying",
+  "5 golden rings",
+  "4 calling birds",
+  "3 French hens",
+  "2 turtle doves",
+  "1 partridge in a pear tree",
+];
+
 //👉 Write the function your CodeWarriors will start with below here:
+export function singTwelveDays(array) {
+  return array;
+}
